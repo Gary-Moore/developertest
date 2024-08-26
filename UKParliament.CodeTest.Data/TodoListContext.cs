07913f17212ea497160c1494f8b3f35847167fe5
Jody Broad <broadj@parliament.uk>
2024-08-26 16:18:22 +0100

    - added extra to do item to db context

--- UKParliament.CodeTest.Data/TodoListContext.cs
+++ UKParliament.CodeTest.Data/TodoListContext.cs
@@ -16,7 +16,12 @@ namespace UKParliament.CodeTest.Data
 
             // Use this method to seed the DB with Todo items, if desired, like so:
             // look at how I am doing datetime here, not sure this is right
-            modelBuilder.Entity<TodoItem>().HasData(new TodoItem { Id = 1, Title = "Project planning", Description = "Write project plan", IsCompleted = false, DueDate = DateTime.Today});
+            modelBuilder.Entity<TodoItem>().HasData(new TodoItem 
+            { Id = 1, Title = "Project planning", Description = "Write project plan", IsCompleted = false, DueDate = DateTime.Today});
+
+            // surely i can pass in a list or something here to add more than one?
+            modelBuilder.Entity<TodoItem>().HasData(new TodoItem
+            { Id = 2, Title = "Refine goals", Description = "Work with stakeholders to refine goals", IsCompleted = false, DueDate = DateTime.Today });
         }
 
         public DbSet<TodoItem> TodoItems { get; set; }
