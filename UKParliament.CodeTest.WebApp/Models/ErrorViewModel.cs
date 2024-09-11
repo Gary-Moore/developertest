a2167012bebec8e86aea8ed948a8b581781364b5
Gary-Moore <garyprm@gmail.com>
2024-09-11 10:35:49 +0100

    initial commit

---
+++ UKParliament.CodeTest.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UKParliament.CodeTest.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
